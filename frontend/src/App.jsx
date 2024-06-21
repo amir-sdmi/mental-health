ccbed62f53c0ad823afcb170e524d379228bdc36
amirrrrrrr7 <amir.sdmi94@gmail.com>
2024-06-21 13:58:09 +0200

    WIP01

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from "react";
+import { UserData } from "./Data";
+import { Bar } from "react-chartjs-2";
 function App() {
-  const [count, setCount] = useState(0)
+  const [userData, setUserData] = useState({
+    labels: [],
+    datasets: [],
+  });
 
+  useEffect(() => {
+    const labels = Object.keys(UserData.Data);
+    const data2015 = labels.map((label) => UserData.Data[label][2015]);
+    const data2016 = labels.map((label) => UserData.Data[label][2016]);
+    const data2017 = labels.map((label) => UserData.Data[label][2017]);
+    setUserData({
+      labels,
+      datasets: [
+        {
+          label: 2015,
+          data: data2015,
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+        },
+        {
+          label: 2016,
+          data: data2016,
+          backgroundColor: "rgba(54, 162, 235, 0.2)",
+          borderColor: "rgba(54, 162, 235, 1)",
+          borderWidth: 1,
+        },
+        {
+          label: 2017,
+          data: data2017,
+          backgroundColor: "rgba(255, 206, 86, 0.2)",
+          borderColor: "rgba(255, 206, 86, 1)",
+          borderWidth: 1,
+        },
+      ],
+    });
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <Bar data={userData} />
+    </div>
+  );
 }
 
-export default App
+export default App;
