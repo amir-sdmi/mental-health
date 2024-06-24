0ccf2505436db4c318f5dac34c61351eb77ec9fe
Ehsan Shahabirad <117647710+EhsanShahabiRad@users.noreply.github.com>
2024-06-24 14:31:06 +0200

    change on JSON data formation
    Changes made on Request_Handler.py to:
    1. send null for missing values
    2. Send JSON response in a format  that the response include all requested columns grouped under their respective categories

--- backend/Request_handler.py
+++ backend/Request_handler.py
@@ -1,89 +1,51 @@
-
-
-# from flask import request
-# from flask_restful import Resource
-# import pandas as pd
-# from services.data_scaling import scale_data_with_global_min_max, global_min, global_max
-
-# def load_data(filepath):
-#     return pd.read_csv(filepath)
-
-# # Load data
-# mental_health_data = load_data('assets/Cleaned_Mental_Health_Data.csv')
-# world_happiness_data = load_data('assets/Cleaned_World_Happiness_Report_2005-2021.csv')
-
-# class DataQuery(Resource):
-#     def get(self, countryName, startDate, endDate):
-#         
-#         query_params = request.args
-#         mh_filtered = mental_health_data[
-#             (mental_health_data['Entity'] == countryName) &
-#             (mental_health_data['Year'].between(int(startDate), int(endDate)))
-#         ].set_index('Year')
-
-#         wh_filtered = world_happiness_data[
-#             (world_happiness_data['Country name'] == countryName) &
-#             (world_happiness_data['Year'].between(int(startDate), int(endDate)))
-#         ].set_index('Year')
-#         mh_columns = set(query_params.keys()).intersection(mh_filtered.columns)
-#         wh_columns = set(query_params.keys()).intersection(wh_filtered.columns)
-        
-#         mh_features = mh_filtered[list(mh_columns)]
-#         wh_features = wh_filtered[list(wh_columns)]
-
-#         combined_features = pd.concat([mh_features, wh_features], axis=1, join='inner')
-#         scaled_data = scale_data_with_global_min_max(combined_features, global_min, global_max)
-#         result_data = {col: scaled_data[col].to_dict() for col in scaled_data.columns}
-
-#         result = {
-#             'Country': countryName,
-#             'Start Year': startDate,
-#             'End Year': endDate,
-#             'Data': result_data
-#         }
-
-#         return result
-
 from flask import request
 from flask_restful import Resource
 import pandas as pd
+import numpy as np
 
 def load_data(filepath):
     return pd.read_csv(filepath)
-
 
 mental_health_data = load_data('assets/Cleaned_Mental_Health_Data.csv')
 world_happiness_data = load_data('assets/Cleaned_World_Happiness_Report_2005-2021.csv')
 
 class DataQuery(Resource):
     def get(self, countryName, startDate, endDate):
- 
         query_params = request.args
+        years = list(range(startDate, endDate + 1))
 
         mh_filtered = mental_health_data[
-            (mental_health_data['Entity'] == countryName) &
-            (mental_health_data['Year'].between(int(startDate), int(endDate)))
-        ].set_index('Year')
+            (mental_health_data['Entity'] == countryName)
+        ].set_index('Year').reindex(years)
 
         wh_filtered = world_happiness_data[
-            (world_happiness_data['Country name'] == countryName) &
-            (world_happiness_data['Year'].between(int(startDate), int(endDate)))
-        ].set_index('Year')
+            (world_happiness_data['Country name'] == countryName)
+        ].set_index('Year').reindex(years)
 
         mh_columns = set(query_params.keys()).intersection(mh_filtered.columns)
         wh_columns = set(query_params.keys()).intersection(wh_filtered.columns)
-        
+
         mh_features = mh_filtered[list(mh_columns)]
         wh_features = wh_filtered[list(wh_columns)]
 
-        combined_features = pd.concat([mh_features, wh_features], axis=1, join='inner')
+        combined_features = pd.concat([mh_features, wh_features], axis=1, join='outer')
 
-        result_data = {col: combined_features[col].to_dict() for col in combined_features.columns}
+        for col in mh_columns.union(wh_columns):
+            if col not in combined_features:
+                combined_features[col] = np.nan
+
+        combined_features = combined_features.fillna(np.nan)
+
+        result_data = {
+            "Disorders": {col: combined_features[col].tolist() for col in mh_columns},
+            "Happiness": {col: combined_features[col].tolist() for col in wh_columns}
+        }
 
         result = {
             'Country': countryName,
             'Start Year': startDate,
             'End Year': endDate,
+            'Years': years,
             'Data': result_data
         }
 
