a836fde16a8faf06c25006ecf04eed9b91744aa0
Ehsan Shahabirad <117647710+EhsanShahabiRad@users.noreply.github.com>
2024-07-04 13:08:31 +0200

    remove comented codes and cleanup

--- backend/app/resources/data_loader.py
+++ backend/app/resources/data_loader.py
@@ -1,30 +1,3 @@
-# from flask import abort
-# import pandas as pd
-# import os
-
-# def load_data(filepath):
-#     try:
-#         return pd.read_csv(filepath)
-#     except FileNotFoundError:
-#         abort(404, description=f"File not found: {filepath}")
-
-# #Load csv files
-# base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'assets'))
-# mental_health_data = load_data(os.path.join(base_path, 'Cleaned_Mental_Health_Data.csv'))
-# world_happiness_data = load_data(os.path.join(base_path, 'Cleaned_World_Happiness_Report_2005-2021.csv'))
-# gdp_data = load_data(os.path.join(base_path, 'GDP_per_cap.csv'))
-
-# #Preprocess GDP data
-# gdp_data = gdp_data.melt(id_vars=['Country-name'], var_name='Year', value_name='GDP')
-# gdp_data['Year'] = gdp_data['Year'].astype(int)
-
-# #list of available countries
-# available_countries = mental_health_data['Country-name'].unique()
-
-
-
-
-
 
 from flask import abort
 import pandas as pd
--- backend/app/services/data_scaling.py
+++ backend/app/services/data_scaling.py
@@ -1,34 +1,3 @@
-# import pandas as pd
-
-# def calculate_percentage_change(data, column):
-#     data[column] = data[column].pct_change() * 100
-#     return data
-
-# def filter_and_prepare_data(query_params, mh_filtered, wh_filtered, gdp_filtered):
-#     if 'GDP' in query_params:
-#         gdp_filtered = calculate_percentage_change(gdp_filtered, 'GDP')
-
-#     mh_columns = set(query_params.keys()).intersection(mh_filtered.columns)
-#     wh_columns = set(query_params.keys()).intersection(wh_filtered.columns)
-#     gdp_column = 'GDP' if 'GDP' in query_params else None
-
-#     mh_features = mh_filtered[list(mh_columns)] if mh_columns else pd.DataFrame()
-#     wh_features = wh_filtered[list(wh_columns)] if wh_columns else pd.DataFrame()
-#     gdp_features = gdp_filtered[[gdp_column]] if gdp_column else pd.DataFrame()
-
-#     return mh_features, wh_features, gdp_features, mh_columns, wh_columns, gdp_column
-
-# def combine_features(mh_features, wh_features, gdp_features, start_year, end_year):
-#     all_years = pd.DataFrame(index=pd.RangeIndex(start_year, end_year + 1))
-#     mh_features = all_years.join(mh_features, how='left').ffill()
-#     wh_features = all_years.join(wh_features, how='left').ffill()
-#     gdp_features = all_years.join(gdp_features, how='left').ffill()
-
-#     combined_features = pd.concat([mh_features, wh_features, gdp_features], axis=1)
-#     combined_features = combined_features.apply(lambda x: x.fillna(x.mean()), axis=0)
-
-#     return combined_features
-
 
 import pandas as pd
 
